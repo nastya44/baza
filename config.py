e7a22aab86f3f9b525648366ccdf02c25c59e5a2
Igor Merkulov <avprevolucion@gmail.com>
2021-11-16 16:47:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 
 class Config():
-    SQLACHEMY_DATABASE_URI = os.environ['MY_RIGHT_DATABASE']
+    SQLACHEMY_DATABASE_URI = os.environ['postgres://iqvhsivywqoojo:b9f5128049d7d0c1b20932bb75b1e952bf733ac9163e49627ca5fe61fd8da375@ec2-52-209-171-51.eu-west-1.compute.amazonaws.com:5432/dc0ptqvl1k76ps']
